cdbd37be489c32809629b9b8d3d98e8bcce713f6
minionswq <minionswq2.2.5.9@gmail.com>
2021-05-21 11:41:11 +0700

    d

--- Source.cpp
+++ Source.cpp
@@ -38,9 +38,9 @@ void ChayChuongTrinh()
 		{
 			if (a[i][j] % 2 != 0)
 			{
-				cout << a[i][j];
+				cout <<"so Le Dau Tien xuat Hien trong mang 2 chieu "<< a[i][j];
 				cout << endl;
-				cout << "hang " << i << "cot" << j;
+				cout << " hang " << i << "cot" << j;
 				return;
 			}
 
